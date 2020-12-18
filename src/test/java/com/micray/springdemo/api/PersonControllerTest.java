c2ad41efac08fa60a48ee942986188c5d5f4df74
mic-ray <michael.ray30@gmx.de>
2020-12-18 12:52:07 +0100

    Added more tests
    Tests:
    - Added more tests to the PersonControllerTest class
    - Testing the 404 return of the Controller, so that a name that is not provided by the (mocked) PersonService should return not found

--- src/test/java/com/micray/springdemo/api/PersonControllerTest.java
+++ src/test/java/com/micray/springdemo/api/PersonControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PersonController.class)
@@ -28,5 +29,24 @@ class PersonControllerTest {
         this.mockMvc.perform(get("/api/v1/person/name?name=Mike"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name").value("Mike"));
+
+        // If a name is passed, that is not provided by the PersonService it should return a 404
+        this.mockMvc.perform(get("/api/v1/person/name?name=Mickey"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Resource not found"));
+    }
+
+    @Test
+    void getPersonByNameShouldReturnNotFound() throws Exception {
+        when(personService.getPersonByName("Mike")).thenReturn(null);
+
+        this.mockMvc.perform(get("/api/v1/person/name?name=Mike")).andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Resource not found"));
+
+        // If a name is passed, that is not provided by the PersonService it should also return a 404
+        this.mockMvc.perform(get("/api/v1/person/name?name=Mickey")).andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Resource not found"));
     }
 }
