547c11e5f664dc3973231e04cb1e533ce4bc1513
mic-ray <michael.ray30@gmx.de>
2020-12-20 22:39:03 +0100

    Added new test
    PersonController test:
    - Added a new test for the getAllPeople method
    - Mock Person objects are created and added to personList, which is to be returned by the mocked PersonService
    - The mock request is performed, the size of the response array and the contained Person names are tested (in order)

--- src/test/java/com/micray/springdemo/api/PersonControllerTest.java
+++ src/test/java/com/micray/springdemo/api/PersonControllerTest.java
@@ -8,8 +8,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -75,5 +78,24 @@ class PersonControllerTest {
         this.mockMvc.perform(get("/api/v1/person/"+id)).andDo(print())
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.message").value("Resource not found"));
+    }
+
+    @Test
+    void getAllPeopleShouldReturnPeople() throws Exception{
+
+        Person person1 = new Person("Mike");
+        Person person2 = new Person("James");
+        List<Person> personList = new ArrayList<>();
+        personList.add(person1);
+        personList.add(person2);
+
+        when(personService.getAllPeople()).thenReturn(personList);
+
+        this.mockMvc.perform(get("/api/v1/person/")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name").value("Mike"))
+                .andExpect(jsonPath("$[1].name").value("James"));
     }
 }
