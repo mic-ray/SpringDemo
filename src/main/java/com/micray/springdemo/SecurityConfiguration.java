5b686c5376ceec9b9e81e631d01c4e36a8905980
mic-ray <michael.ray30@gmx.de>
2020-12-23 19:54:11 +0100

    Added Spring Security to project
    Spring Security:
    - Added dependencies of Spring security packages in pom.xml
    - Created a SecurityConfiguration file, that configures the security of the application
    - Configured the security to authenticate the /api/v1/person endpoint, specifically the DELETE method using HTTP Basic authentication

---
+++ src/main/java/com/micray/springdemo/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.micray.springdemo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/api/v1/**")
+                .authorizeRequests()
+                .antMatchers(HttpMethod.DELETE, "/api/v1/person").authenticated();
+    }
+}
