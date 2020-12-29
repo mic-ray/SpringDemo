f91a37248b6c5a188c7f6fdf74375962e9f654d3
mic-ray <michael.ray30@gmx.de>
2020-12-29 13:47:01 +0100

    Added test for view controller
    GreetingViewController test:
    - Added test for the greeting method of the GreetingViewController
    - A get request is mocked and the returned view and correct model attributes (provided in the request parameters) are checked

---
+++ src/test/java/com/micray/springdemo/GreetingViewControllerTest.java
@@ -0,0 +1,28 @@
+package com.micray.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(GreetingViewController.class)
+class GreetingViewControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void greetingShouldReturnCustomModelAndView() throws Exception {
+        this.mockMvc
+                .perform(get("/greeting?name=Mike")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("greeting"))
+                .andExpect(model().attributeExists("name"))
+                .andExpect(model().attribute("name", "Mike"));
+    }
+}
